c1e0932f798bafcdaeb24ce3bcfc0660e500524c
Trdat Mkrtchyan <zeffirsky@yandex-team.ru>
2016-12-07 17:33:46 +0300

    Added secrets

---
+++ jazz/secrets.py
@@ -0,0 +1,11 @@
+TOKEN='141154212:AAGUQFA93xCOVEaoCTG5SUHB766p17OQ-Ak'
+ABSOLUTE_TOTAL_ADMINS=[
+	{
+		'u': 'lolichan',
+		'p': 'уле=еле'
+	},
+	{
+		'u': 'zefirka',
+		'p': 'кто умный говна хуюмный'
+	}
+]
